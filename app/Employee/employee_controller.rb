9a5f6634394d08d4ee02dcf95d04410e997dab8d
Derek Warren <derek.sa.warren@gmail.com>
2013-10-01 17:09:18 -0400

    Adding new employee w/ camera

--- app/Employee/employee_controller.rb
+++ app/Employee/employee_controller.rb
@@ -1,18 +1,25 @@
 require 'rho/rhocontroller'
 require 'helpers/browser_helper'
+require 'Date'
 
 class EmployeeController < Rho::RhoController
   include BrowserHelper
 
   # GET /Employee
   def index
-    @employees = Employee.find(:all)
+    @employee = Employee.find(:all)
     render :back => '/app'
   end
 
   # GET /Employee/{1}
   def show
-    @employee = Employee.find(@params['id'])
+    #Temporary check TO BE IMPROVED and Modulized
+    if @params['id'].nil?
+      @employee = Employee.find($holdModelID)
+    else
+      @employee = Employee.find(@params['id'])
+    end
+
     if @employee
       render :action => :show, :back => url_for(:action => :index)
     else
@@ -37,10 +44,70 @@ class EmployeeController < Rho::RhoController
   end
 
   # POST /Employee/create
+  #Flags for Validation with binary flags firstName, lastName, phone, startDate, endDate, status.
+  #EX: startDate and endDate are in incorrect format.
   def create
-    @employee = Employee.create(@params['employee'])
-    $holdModelID = @employee.object
-    redirect :action => :preparePic
+    puts @params
+    # Check if the fields are valid or not
+    @invalidFields = []
+    @params['employee'].each do |key, value|
+      puts key
+      #Check for empty value
+      if value.empty?
+        puts "EMPTY PARAM"
+        @invalidFields << {key => value}
+        #WebView.executeJavascript('invalidForm();')
+        #break
+      end
+      case key
+      when "firstName", "lastName"
+        # Check to see if the form value is valid otherwise return error.
+        # Regular expression for Digits and Whitespace only
+        if (value =~ /[\d\W]+/).nil?
+          puts "#{value} OK"
+        else
+          @invalidFields << {key => value}
+        end
+      when "phone"
+        # Valid only with number between 10 - 12 digits
+        # Regular expression for Numbers only
+        #if (key =~ /[\D]+/).nil?
+        if (value =~ /(?:\+?|\b)[0-9]{10,12}\b/) == 0
+          puts "#{value} OK"
+        else
+          @invalidFields << {key => value}
+        end
+      when "startDate"
+        begin
+          d = Date.parse(value)
+        rescue Exception => e
+          @invalidFields << {key => value}
+          puts "invalid Start Date"
+        end
+      when "endDate"
+        begin
+          d = Date.parse(value)
+        rescue Exception => e
+          @invalidFields << {key => value}
+          puts "invalid End Date"
+        end
+      when "status"
+        puts "Status Check"
+      else
+        puts "I shouldn't be SEEN!"
+      end        
+      puts @invalidFields
+    end
+
+    if @invalidFields.empty?
+      @employee = Employee.create(@params['employee'])
+      $holdModelID = @employee.object
+      redirect :action => :preparePic
+    else
+      #WebView.executeJavascript('invalidForm(#{@invalidFields});')
+      WebView.executeJavascript('invalidForm();')
+      redirect :action => :new
+    end
   end
 
   # POST /Employee/{1}/update
@@ -56,40 +123,27 @@ class EmployeeController < Rho::RhoController
     @employee.destroy if @employee
     redirect :action => :index  
   end
-
-  def preview_image
-    #@employee = Employee.find(@params['id'])
-    render :action => :postviewPic
-  end
-  
   
 ### Camera Related Methods ###
   
   def load_camera
-    #objectID = $holdModelID
     puts "!!! Starting Camera !!!"
-    #puts objectID
     #Rho::Camera.takePicture({}, :camera_callback)          API when Camera implementation is fixed
     Camera::take_picture(url_for(:action => :camera_callback))#, :id => $holdModelID))
   end
 
   def camera_callback
-    #puts "HHHHHHHHHHHHHHHHHHHH+++++ #{@params['id']}"
-    puts "HHHHHHHHHHHHHHHHHHHH+++++ #{$holdModelID}"
     #@employee = Employee.find(@params['id'])
     @employee = Employee.find($holdModelID)
-    puts @params['status']
-    puts @params['image_uri']
-    puts @employee
     
     #Save the image to the New Employee/Guest
     if @params['status'] == 'ok'
       puts "Camera Picture Successful!"
-      #@employee.update_attributes({:image => @params['image_uri']})
       $imageHolder = @params['image_uri']
-      puts @employee
-      puts Rho::Application.expandDatabaseBlobFilePath($imageHolder)
-      Rho::WebView.navigate(url_for(:action => :preview_image)) #, :id => @employee.object))
+      #puts Rho::Application.expandDatabaseBlobFilePath($imageHolder)
+      #Rho::WebView.navigate(url_for(:action => :preview_image)) #, :id => @employee.object))
+      Rho::WebView.navigate(url_for(:action => :preview_image, :acceptImg => @params['image_uri']))
+      #redirect :action => :previewPic
     elsif @params['status'] == 'cancel'
       Rho::Notification.showPopup({
         :message => "Image Taking Canceled",
@@ -109,7 +163,28 @@ class EmployeeController < Rho::RhoController
     end
   end
 
-  def load_picture
-    
+### Image saving and displaying
+
+  def preview_image
+    #@employee = Employee.find(@params['id'])
+    render :action => :previewPic
+  end
+
+  def assign_image
+    #TO BE Optimized by finding first match
+    @employee = Employee.find($holdModelID)
+    puts @employee
+    @employee.update_attributes({:image => $imageHolder})
+    puts @employee
+
+    #Clear globals for collection and open listing.
+    clear_camera_vars
+    redirect :action => :index
+  end
+
+  def clear_camera_vars
+    $holdModelID = nil
+    $imageHolder = nil
+    return
   end
 end
---
+++ app/README.md
@@ -0,0 +1,15 @@
+Readme file for the New Employee/Guest application
+
+The goal of this application is to link enterprise devices with consumer devices.
+
+A manager takes new employee information on either MC40/ET1/MC??(WM) or Consumer device. The information will then be displayed on a SB1 or other deivce issued to the new employee.
+
+Current Functionality:
+Add User information and Picture
+
+Needed Improvements:
+Native Date picker in Android
+Page sizing due to keyboard (Android)
+Formal form validation
+Text fields cleared on invalid form input
+Resize image after taking
